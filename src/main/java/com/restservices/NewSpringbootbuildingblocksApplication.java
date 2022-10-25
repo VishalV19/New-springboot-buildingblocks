13333e80f4f1cfa6b68157826c93fb604ffbe32c
Vishal Singh <vishalsingh.vh99@gmail.com>
2022-10-25 19:31:18 +0530

    First Commit

---
+++ src/main/java/com/restservices/NewSpringbootbuildingblocksApplication.java
@@ -0,0 +1,13 @@
+package com.restservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewSpringbootbuildingblocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewSpringbootbuildingblocksApplication.class, args);
+	}
+
+}
