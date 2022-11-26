5cd734b6cac663c6d5e6369ec9ddb171fb47df1b
Vishal Singh <vishalsingh.vh99@gmail.com>
2022-11-26 14:15:45 +0530

    i18 - First Commit

--- src/main/java/com/restservices/Hello/HelloWorldController.java
+++ src/main/java/com/restservices/Hello/HelloWorldController.java
@@ -1,13 +1,17 @@
 package com.restservices.Hello;
 
+import java.util.Locale;
+
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 //Controller
 @RestController
 public class HelloWorldController {
+	
+	private ResourceBundleMessageSource messageSource;
 
 // Simple Method
 // URI - 
@@ -24,7 +28,15 @@ public class HelloWorldController {
 	public UserDetails helloWorldBean() {
 		return new UserDetails("Vishal", "Singh","Lucknow");
 	}
+	
+	@GetMapping("/hello-int")
+	public String getMessageInI18Nformat(@RequestHeader (name = "Accept-Language", required=false) String locale) {
+		return messageSource.getMessage("Label.hello", null, new Locale(locale));
+	}
 
+	public String getMessagesInI18NFormat() {
+		return "Hello world I18N";
+	}
 }
 
 
--- src/main/java/com/restservices/NewSpringbootbuildingblocksApplication.java
+++ src/main/java/com/restservices/NewSpringbootbuildingblocksApplication.java
@@ -2,6 +2,14 @@ package com.restservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+
+
+
+
+
+
 
 //
 @SpringBootApplication
@@ -10,5 +18,23 @@ public class NewSpringbootbuildingblocksApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(NewSpringbootbuildingblocksApplication.class, args);
 	}
+	
+	
+	
+//	@Bean
+//	public LocaleResolver localeResolver() {
+//		org.springframework.web.servlet.LocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+//		((AbstractResourceBasedMessageSource) localeResolver).setDefaultLocale(Locale.US);
+//		return (org.hibernate.validator.spi.messageinterpolation.LocaleResolver) localeResolver;
+//	}
+	@Bean
+	public ResourceBundleMessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("messages");
+		return messageSource;
+				
+		
+	} 
+	
 
 }
